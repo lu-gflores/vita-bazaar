16994a64ce76fdb0e4e62b2447d87c91cd9963f8
George Flores <georgeflores886@gmail.com>
2021-07-05 16:10:00 -0400

    created models for data

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,9 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useCreateIndex: true
         })
-        console.log(`MongoDb connected: ${conn.connection.host}`)
+        console.log(`MongoDb connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
-        console.error(`Error: ${error.mongoose}`)
+        console.error(`Error: ${error.mongoose}`.red.underline.bold)
         process.exit(1)
     }
 }
