f6209dbaef917e5d6651e5c1efd67947c902faca
George Flores <georgeflores886@gmail.com>
2021-07-06 11:26:21 -0400

    created product action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,6 +1,5 @@
-import { PRODUCT_LIST_REQUEST } from "../constants/productConstants"
-import { PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
-import { PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
