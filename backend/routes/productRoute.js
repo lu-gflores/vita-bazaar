7731bcfab1a2945a5fe6d4ceac20543d4a04ca0a
George Flores <georgeflores886@gmail.com>
2021-07-06 10:29:14 -0400

    created products router path

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+
+// fetches all products
+// get /api/products
+// public access
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
+
+//fetches a single product
+// get /api/products/:id
+// public access
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) res.json(product)
+    else res.status(404).json({ message: 'Product not found' })
+}))
+
+export default router
