40ffec46be1383f93cb6af892b2a0c62a4c0ec3e
George Flores <georgeflores886@gmail.com>
2021-07-06 10:54:34 -0400

    created error handling for status 404 and 505

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoute.js'
@@ -15,6 +17,12 @@ app.get('/', (req, res) => {
     res.send('API is running')
 })
 app.use('/api/products', productRoutes)
+
+//error handling for 404
+app.use(notFound)
+
+//eror handling for 500
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 4000
 
