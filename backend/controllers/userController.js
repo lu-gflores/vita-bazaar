9219fb1963c9f4f67a65da986464228fc4f0c563
George Flores <georgeflores886@gmail.com>
2021-07-14 14:32:43 -0400

    created auth middleware for token auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,7 +26,19 @@ const authUser = asyncHandler(async (req, res) => {
 // get /api/users/profile
 // private access
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('Success')
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
 })
 
 export { authUser, getUserProfile }
