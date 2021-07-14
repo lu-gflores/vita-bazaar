745c190415cb3fe07081695b9634e845d54ad2a3
George Flores <georgeflores886@gmail.com>
2021-07-14 14:04:34 -0400

    created user controller and auth

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,20 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoute.js'
+import userRoute from './routes/userRoute.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoute)
 
 //error handling for 404
 app.use(notFound)
