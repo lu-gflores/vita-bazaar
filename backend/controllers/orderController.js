f538883e4e00db7e0ee970cca98169d76491f1d9
George Flores <georgeflores886@gmail.com>
2021-07-30 10:55:44 -0400

    added paid controller and route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -47,6 +47,28 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+// update order to paid
+// get /api/orders/:id/pay
+// private access
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
 
 
-export { addOrderItems, getOrderById }
+export { addOrderItems, getOrderById, updateOrderToPaid }
