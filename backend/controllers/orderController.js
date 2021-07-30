a56454cc54d41936bd1724af63ebaf392d76becd
George Flores <georgeflores886@gmail.com>
2021-07-30 09:43:27 -0400

    created get order by id endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -31,4 +31,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems }
+
+// get order by id
+// get /api/orders/:id
+// private access
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name email')
+
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+
+
+export { addOrderItems, getOrderById }
